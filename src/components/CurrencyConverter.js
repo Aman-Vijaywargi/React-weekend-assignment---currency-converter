65ec27e7b127856b0ab1212f38512674fbcaa43b
AmanFinisher <64026019+AmanFinisher@users.noreply.github.com>
2021-06-19 20:46:41 +0530

    Update CurrencyConverter.js

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,39 +1,91 @@
-import React from 'react'
-import './CurrencyConverter.css';
+import React, { useState, useEffect } from 'react'
 
-export default function CurrencyConverter(props) {
-    const{currencyOptions,
-      currencyOptions1,
-    selectedCurrency,
-    selectedCurrency2,
-    onChangeCurrency,
-    onChangeCurrency2,
-    amount,amount1,
-    onChangeAmount,
-    onChangeAmount1
-   }=props
+export default function CurrencyConverter() {
+
+    const [baseCurrency, setBaseCurrency] = useState("USD");
+    const [convertCurrency, setConvertedCurrency] = useState("IND");
+    const [baseAmount, setBaseAmount] = useState(10);
+    const rates = [];
+    const currencies = [];
+
+    useEffect(() =>
+    {
+        callAPI(baseCurrency);
+    },[])
+
+    function changeBaseCurrency(e)
+    {
+        setBaseCurrency(e.target.value);
+        callAPI(e.target.value);
+    }
+
+    function changeConvertToCurrency(e)
+    {
+        setConvertedCurrency(e.target.value);
+        callAPI(e.target.value);
+    }
+
+    function changeBaseAmount(e)
+    {
+        baseAmount(e.target.value);
+    }
+
+    function callAPI(base) 
+    {
+        const api = `https://api.exchangeratesapi.io/latest?base=${base}`;
+        fetch(api).then(result =>{
+            console.log(result);
+            return result.json();
+        }
+           ).then(data=>
+            {
+                console.log( data['rates']);
+                rates = data['rates'],
+                currencies = Object.keys(data['rates']).sort()
+               
+            });
+    }
+
+   const  getConvertedCurrency = (baseAmount,convertCurrency,rates) =>
+    {
+        return Number.parseFloat(baseAmount * rates[convertCurrency]).toFixed(3);
+
+    }
+    const result = getConvertedCurrency(baseAmount, convertCurrency, rates);
+    
+
+    const currencyOtpions = currencies.map(currency =>
+        <option key={currency} value={currency}> {currency} </option>      
+      );
     return (
         <div>
-            <h1>Currency Converter</h1>
-            <select value={selectedCurrency} onChange={onChangeCurrency} >
-           
-            {currencyOptions.map(option => (
-          <option key={option} value={option}>{option}</option>
-        ))} 
-            </select>
-            <input type="number" value={amount} onChange={onChangeAmount}/>
+            <div>
+                <select
+                    value ={baseCurrency}
+                    onChange={changeBaseCurrency}>
+                    {currencyOtpions}
+                    <option>{baseCurrency}</option>
+                    </select>
 
-
-            <div className="second">
-            <select  value={selectedCurrency2} onChange={onChangeCurrency2}>
-            
-            {currencyOptions1.map(options => (
-          <option key={options} value={options}>{options}</option>
-        ))} 
-            </select>
-            <input type="number"  value={amount1} onChange={onChangeAmount1}/>
+                <input
+                    type={Number}
+                    id="base-Amount"
+                    defaultValue={setBaseAmount}
+                    onChange={changeBaseAmount}
+                />
+              
             </div>
-            
+
+           <div >
+            <select value={convertCurrency} onChange={changeConvertToCurrency}>
+                {currencyOtpions}
+            </select>
+            <input
+                id="result-text"
+                value = {result}
+               
+            />
+           </div>
         </div>
-    )
+    );
 }
